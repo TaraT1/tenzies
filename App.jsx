b3f608c33add6fb59fe569fd8e4e8cda6b420295
TaraT1 <tarathierry@gmail.com>
2025-11-03 21:36:23 -0500

    Add roll die functionality; generate objects for die

--- App.jsx
+++ App.jsx
@@ -2,16 +2,26 @@ import { useState } from "react"
 import Die from "./Die"
 
 export default function App() {
+    /**
+     * array of numbers ---> array of objects
+     * object is comprised of { value: <rando num>, isHeld: false }
+     * Correct breaking code
+     */
+
+
     const [dice, setDice] = useState(allNewDice())
 
     function allNewDice() {
         //array of 10 random nums - Functional
         return new Array(10)
             .fill(0)
-            .map(() => Math.ceil(Math.random() * 6))
+            .map(() => ({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false
+        }))
     }
     //map over dice; Can also map in <main> 
-    const diceElements = dice.map(num => <Die value={num} />)
+    const diceElements = dice.map(dieObj => <Die value={dieObj.value} />)
 
     function rollDice() {
         setDice(allNewDice)
